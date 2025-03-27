8ca6cd5b580a8272f0d62c27690ce96f73e76a89
felipevieira07 <feligavime@gmail.com>
2025-03-26 22:19:26 -0300

    att trote

---
+++ trote/js/scripts.js
@@ -0,0 +1,28 @@
+function calcular(){
+    //declarar variável sem tipo
+    let soma = 0
+    //recupera o valor da ação social digitado
+    let acaoSocial = document.getElementById("acaoSocial").value
+    soma = soma + Number(acaoSocial)
+    //recupera o valor da homenagem digitado
+    let homenagem = document.getElementById("homenagem").value
+    soma = soma + Number(homenagem)
+    //recupera a qtde de litros de leite
+    let leite= document.getElementById("leite").value
+    soma = soma + (2 * Number(leite))
+    //recupera a qtde de kits avulsos
+    let kitAvulso= document.getElementById("kitAvulso").value
+    soma = soma + (30 * kitAvulso)
+    //recupera a qtde de latas de suplemento
+    let suplementoAvulso= document.getElementById("suplementoAvulso").value
+    soma = soma + (15 * Number(suplementoAvulso))
+    //recupera a qtde de pacote de 5kg
+    let pacote5kgAvulso= document.getElementById("pacote5kgAvulso").value
+    soma= soma + (5 * Number(pacote5kgAvulso))
+    //recupera a qtde de pacote de 1kg
+    let pacote1kgAvulso= document.getElementById("pacote1kgAvulso").value
+    soma= soma + (1 * Number(pacote1kgAvulso))
+    //devolve o resultado para o HTML
+    document.getElementById("soma").innerHTML=soma.toFixed(2)
+
+}
