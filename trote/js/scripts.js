5b09bc44e411e1f7631af6f0d8abffe1931097c2
felipevieira07 <feligavime@gmail.com>
2025-04-02 22:06:34 -0300

    atttrote2

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -13,40 +13,28 @@ function calcular(){
     //recupera a qtde de kits
     let kit= document.getElementById("kit").value
     soma = soma + (30 * kit)
+     //recupera a qtde de latas de suplemento
+     let suplemento= document.getElementById("suplemento").value
+     soma = soma + (15 * Number(suplemento))
     //recupera a cor da equipe
     let equipe= document.getElementById("equipe").value
     let pontosKit = 0 //guarda pontuação de kit 
     if (equipe == "Laranja"){
         //equipe é laranja
-        if (kit >= 97 )
-            pontosKit = 5000 + ((kit - 97) * 30)
+        if (kit >= 97 && suplemento >= 49)
+            pontosKit = 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
         }
-        else if (kit >= 78){
-            pontosKit = 4000 + ((kit - 79) * 30)
+        else if (kit >= 78 && suplemento >= 39){
+            pontosKit = 4000 + ((kit - 79) * 30) + ((suplemento - 39) * 15)
         }
-        else if (kit >= 49){
-            pontosKit = 2500 + ((kit - 49) * 30)
+        else if (kit >= 49 && suplemento >= 25){ 
+            pontosKit = 2500 + ((kit - 49) * 30) + ((suplemento - 25) * 15)
         }
-        else if (kit >= 19){
-            pontosKit = 1000 + ((kit - 19) * 30)
+        else if (kit >= 19 && suplemento >= 10){
+            pontosKit = 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
     }
     alert(pontosKit)
     soma = soma + pontosKit
-    //recupera a qtde de latas de suplemento
-    let suplemento= document.getElementById("suplemento").value
-    soma = soma + (15 * Number(suplemento))
-    //recupera a qtde de pacote de 5kg
-    let pacote5kg= document.getElementById("pacote5kg").value
-    soma= soma + (5 * Number(pacote5kg))
-    //recupera a qtde de pacote de 1kg
-    let pacote1kg= document.getElementById("pacote1kg").value
-    soma= soma + (1 * Number(pacote1kg))
-    //recupera a qtde de pacote de 400/500g
-    let pacotedemacarrao= document.getElementById("pacotedemacarrao").value
-    soma= soma + (0.5 * Number(pacotedemacarrao))
-    //recupera a qtde de oleo
-    let oleo= document.getElementById("oleo").value
-    soma= soma + (1 * Number(oleo))
     //devolve o resultado para o HTML
     document.getElementById("soma").innerHTML=soma.toFixed(2)
 
