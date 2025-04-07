09515357e0ffb39eca434b59da8e18c6fc82b955
felipevieira07 <feligavime@gmail.com>
2025-04-07 20:32:57 -0300

    atttrote1

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,27 +1,25 @@
 function calcular(){
     //declarar variável sem tipo
-    let soma = 0
+    let soma = 0, acaoSocial ,homenagem,leite,kit,equipe,suplemento, pontosKitSup1 = 0,sangue, pontosSangue = 0
     //recupera o valor da ação social digitado
-    let acaoSocial = document.getElementById("acaoSocial").value
+    acaoSocial = document.getElementById("acaoSocial").value
     soma = soma + Number(acaoSocial)
     //recupera o valor da homenagem digitado
-    let homenagem = document.getElementById("homenagem").value
+    homenagem = document.getElementById("homenagem").value
     soma = soma + Number(homenagem)
     //recupera a qtde de litros de leite
-    let leite= document.getElementById("leite").value
+    leite= document.getElementById("leite").value
     soma = soma + (2 * Number(leite))
     //recupera a qtde de kits
-    let kit= document.getElementById("kit").value
-    soma = soma + (30 * kit)
-     //recupera a qtde de latas de suplemento
-     let suplemento= document.getElementById("suplemento").value
-     soma = soma + (15 * Number(suplemento))
+    kit= Number(document.getElementById("kit").value)
     //recupera a cor da equipe
-    let equipe= document.getElementById("equipe").value
-    let pontosKit = 0 //guarda pontuação de kit 
+    equipe= document.getElementById("equipe").value
+    //recupera a qtde de latas de suplemento
+    suplemento= document.getElementById("suplemento").value
+    pontosKitSup1 = 0 //guarda pontuação de kit 
     if (equipe == "Laranja"){
         //equipe é laranja
-        if (kit >= 97 && suplemento >= 49)
+        if (kit >= 97 && suplemento >= 49){
             pontosKit = 5000 + ((kit - 97) * 30) + ((suplemento - 49) * 15)
         }
         else if (kit >= 78 && suplemento >= 39){
@@ -32,10 +30,64 @@ function calcular(){
         }
         else if (kit >= 19 && suplemento >= 10){
             pontosKit = 1000 + ((kit - 19) * 30) + ((suplemento - 10) * 15)
+        }
+    } 
+    else if(equipe == "Preta"){
+
     }
-    alert(pontosKit)
-    soma = soma + pontosKit
+    else if (equipe == "Roxa"){
+
+    }
+    else if (equipe == "Verde"){
+
+    }
+
+    else {//assume que a equipe é vermelha
+
+    }
+    soma = soma + pontosKitSup1
+
+    //sangue
+    sangue = Number(document.getElementById("sangue"))
+
+    if(equipe == "Laranja"){
+        if (sangue >= 49){
+            pontosSangue = 2500 + (sangue- 49) * 20
+        }
+        else{
+            pontosSangue = sangue * 20
+        }
+    }
+    else if (equipe == "Preta"){
+        if(sangue >= 52){
+            pontosSangue = 2500 + (sangue - 52) * 20
+        }
+        else{
+            pontosSangue = samgue *20
+        }
+    }
+    else if (equipe == "Roxa"){
+        if(sangue >= 51){
+            pontosSangue = 2500 + (sangue - 51) * 20
+        }
+        else{
+            pontosSangue = sangue * 20
+        }
+    }
+    else if (equipe == "Verde"){
+        if (sangue >= 44){
+
+        }
+        else{
+            pontosSangue = sangue * 20
+        }
+    }
+    else {
+        if (sangue >= 47){
+            pontosSangue = 2500 + (sangue - 47) * 200
+        }
+    }    
     //devolve o resultado para o HTML
-    document.getElementById("soma").innerHTML=soma.toFixed(2)
+    document.getElementById("soma").innerHTML= `R$ .${soma.toFixed(2)}`
 
 }
